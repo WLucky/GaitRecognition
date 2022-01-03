aca1b4c09fae286541cfc364cc7a313ab9432038
s70049 <s70049@qq.com>
2022-01-03 21:20:31 +0800

    work version3 --wpj

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
 parser.add_argument('--weight_decay', default=0.0, type=float, help='weight decay')
 parser.add_argument('--test_iter', type=int, default=200, help="iter to test")
 parser.add_argument('--train_batch', default='4,8', help='default: 4 label, 8 sample for each label')
-parser.add_argument('--test_batch', default='16', help='test sample batch')
+parser.add_argument('--test_batch', type=int, default='16', help='test sample batch')
 
 ############################## log config ################################
 parser.add_argument('--log_to_file', action='store_true', help="log to file")
@@ -46,8 +46,6 @@ if __name__ == '__main__':
     msg_mgr = get_msg_mgr()
     msg_mgr.init_manager(save_path, args.log_to_file, args.log_iter, 0)
     msg_mgr.log_info(args)
-
-    cfgs = config_loader(args.cfgs)
 
     model = gaitPart()
     model.cuda()
